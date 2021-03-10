d76079979a65877fbafa46e91deb14acd41148c5
Stefano Patti <mastepa@hotmail.com>
2021-03-10 21:33:03 +0100

    ex DONE
    tasto annulla tuttavia non svuota i campi

--- js/script.js
+++ js/script.js
@@ -16,28 +16,39 @@ generaBtn.addEventListener('click', function(){
 
   if (nome !== '', km > 0, eta !== '') {
 
-    costoTotale = costo * km;
-
-    // blocco di sconti
-    document.getElementById('prezzo').innerHTML = costoTotale;
-
-    // blocco cp
-
-    var cp = Math.floor(Math.random() * (100000-90000)) + 90000;
-    document.getElementById('cp').innerHTML = cp;
+    // blocco Nome
+    document.getElementById('pax').innerHTML = nome;
 
     // blocco Carrozza
-
     var carrozza = Math.floor(Math.random() * 10) + 1;
     document.getElementById('carrozza').innerHTML = carrozza;
 
+    // blocco cp
+    var cp = Math.floor(Math.random() * (100000-90000)) + 90000;
+    document.getElementById('cp').innerHTML = cp;
+
+    // blocco di sconti
+    costoTotale = costo * km;
+    if (eta == 1){
+      costoTotale = costoTotale / 100 * 80;
+        document.getElementById('speciale').innerHTML = "Sconto Minorenne";
+    } else if (eta == 3){
+      costoTotale = costoTotale / 100 * 60;
+      document.getElementById('speciale').innerHTML = "Sconto Over 65";
+    } else{
+      document.getElementById('speciale').innerHTML = "Nessuno sconto applicato";
+    }
+    document.getElementById('prezzo').innerHTML = costoTotale + " Euro";
 
   } else{
-
+    alert("Devi copilare tutti i campi!!");
   }
-
 });
 
 annullaBtn.addEventListener('click', function(){
-  console.log('ho cliccato su annulla');
+  // console.log('ho cliccato su annulla');
+   document.getElementById('nome').innerHTML = "";
+   document.getElementById('km').innerHTML = "";
+   // non funziona
+
 });
